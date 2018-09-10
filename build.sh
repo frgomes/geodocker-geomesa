2e1ef6a710b94c259314cf93029bb5fc84d5c092
Richard Gomes <rgomes.info@gmail.com>
2018-09-10 20:36:02 +0100

    Expose port 9097; adjust versions; usability improvements.

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 usage() { echo "Usage: $0 [-t <tag>] [-g <geomesa.version>] [-a <accumulo.version>]" 1>&2; exit 1; }
 
-ACCUMULO_VERSION=1.9.0
-GEOMESA_VERSION=2.0.1
+ACCUMULO_VERSION=1.9.2
+GEOMESA_VERSION=2.0.2
+HADOOP_VERSION=2.7.3
+BIGTOP_VERSION=1.2.1
 
 while getopts ":t:g:a:" o; do
     case "${o}" in
@@ -25,11 +27,19 @@ shift $((OPTIND-1))
 
 if [ -z "$TAG" ]; then TAG=geomesa-${GEOMESA_VERSION}-accumulo-${ACCUMULO_VERSION}; fi
 
-if [[ $ACCUMULO_VERSION == 1.8* ]]; then THRIFT_VERSION=0.9.3; else THRIFT_VERSION=0.9.2; fi
+if [ $ACCUMULO_VERSION \> 1.8* ]; then THRIFT_VERSION=0.9.3; else THRIFT_VERSION=0.9.2; fi
 
-echo "GEOMESA_VERSION = ${GEOMESA_VERSION}"
+echo "GEOMESA_VERSION  = ${GEOMESA_VERSION}"
 echo "ACCUMULO_VERSION = ${ACCUMULO_VERSION}"
+echo "THRIFT_VERSION   = ${THRIFT_VERSION}"
+echo "HADOOP_VERSION   = ${HADOOP_VERSION}"
+echo "BIGTOP_VERSION   = ${BIGTOP_VERSION}"
 echo "TAG = ${TAG}"
 
-export ACCUMULO_VERSION GEOMESA_VERSION TAG THRIFT_VERSION
-make publish
+export ACCUMULO_VERSION GEOMESA_VERSION HADOOP_VERSION TAG THRIFT_VERSION BIGTOP_VERSION
+
+if [ $# == 0 ] ;then
+  make publish
+else
+  make $*
+fi
